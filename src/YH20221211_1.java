aab27b9d50e402d84d2e9157a6177627ca84107d
KimYoungHyo <jenycom10@naver.com>
2022-12-11 21:19:36 +0900

    First print out my console with comment

---
+++ src/YH20221211_1.java
@@ -0,0 +1,15 @@
+
+public class YH20221211_1 { //이클래스에대한 접근제어 설정
+
+public static void main(String[] args) {
+/*public은 접근제어자, 어느곳에서던 해당 객체 참조가능 private와 반대개념
+ * static 이 함수는 정적함수라 static이 아닌 다른 객체에서 호출 불가능
+ * void 리턴되는값이 없다는 뜻, int로 바꾸면 실행 후 정수형데이터 리턴
+ * main(String[] args) main으로 String인덱스를 만들어 선언 args=변수명 
+ * = 문자열데이터가 들어가는 공간 생성
+*/
+
+		System.out.println("hello World!"); //System.out.println -> 문자열 출력 ln<-출력후 줄바꿈
+		
+	}
+}
